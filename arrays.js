ebf38bec8e0a1e04d6cdfe9bbe7f77c8fa154b89
Eugenio Gonzalez <e.gzzart@gmail.com>
2018-09-18 19:31:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+addElementToBeginningOfArray(arr, data){
+  var arr2 = arr
+  arr2.unshift(data)
+  return arr2
+}
+
+destructivelyAddElementToBeginningOfArray(arr, data){
+  arr.unshift(data)
+  return arr
+}
+
+addElementToEndOfArray(arr, data){
+  var arr2 = arr
+  arr2.push(data)
+  return arr2
+}
+
+destructivelyAddElementToEndOfArray(arr, data){
+  arr.push(data)
+  return arr
+}
+
+accessElementInArray(arr, i){
+  return arr[i]
+}
+
+destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift()
+  return arr
+}
+
+removeElementFromBeginningOfArray(arr){
+  var arr2 = arr
+  arr2.shift()
+  return arr2
+}
+
+removeElementFromBeginningOfArray(arr){
+  var arr2 = arr
+  arr2.pop()
+  return arr2
+}
+
+destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.pop()
+  return arr
+}
